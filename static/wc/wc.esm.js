4ca8ceb74a3e6d5fd6e35a7cf796f14cdb485939
Craig Doremus <cdoremus@gmail.com>
2024-01-16 21:54:54 -0500

    Updated media query to 699px on mobile browsers

--- static/wc/wc.esm.js
+++ static/wc/wc.esm.js
@@ -761,7 +761,7 @@ var EmojiPickerWrapper = class extends HTMLElement {
   connectedCallback() {
     let emoji = "";
     this.innerHTML = `
-      <div style="display: flex; gap: 4px; border: 2px solid black; border-radius: 10px; margin: 10px 5px; padding: 5px 10px;">
+      <div>
         <div>
           <emoji-picker></emoji-picker>
         </div>
